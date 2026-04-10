ba08fe87d9cc1d008fee3a0329e004cebd41d905
Aburiru <jibrilliangilangs+github@gmail.com>
2026-04-10 21:24:27 +0800

    Added ASCII Art

--- ScreenProjectionMenu.bat
+++ ScreenProjectionMenu.bat
@@ -1,5 +1,12 @@
 @echo off
+setlocal EnableExtensions EnableDelayedExpansion
 cls
+color A
+
+:: Configuration
+set "SCRCPY_DIR=C:\Users\abril\Documents\SCRCPY\scrcpy-win64-v3.3.3"
+set "SCRCPY_EXE=%SCRCPY_DIR%\scrcpy.exe"
+
 goto ready
 
 :ready
@@ -9,98 +16,137 @@ goto menu
 
 :menu
 cls
-echo ====================================
-echo  Projection Control Menu
-echo ====================================
-echo  Hello Master, What mode are you gonna pick today?
-echo  1. Activate Screen Mirroring
-echo  2. Activate OTG Mode
-echo  3. Activate New Window
-echo  4. Exit
-set /p "menu=~ Enter Number Mode ~: "
+chcp 65001 >nul
+type "%~dp0art.txt"
+echo.
+echo   ---------------------------------------------------------------------------------
+echo                               Developed by: Abril
+echo   ---------------------------------------------------------------------------------
+echo.
+echo   [1] Activate Screen Mirroring
+echo   [2] Activate OTG Mode
+echo   [3] Activate New Window (App Specific)
+echo   [4] Exit
+echo.
+set /p "menu=  ~ Master, please enter your choice ~ : "
 if "%menu%" == "1" goto sm
 if "%menu%" == "2" goto otg
 if "%menu%" == "3" goto nw
 if "%menu%" == "4" exit
 if "%menu%" == "" goto menu
-echo Invalid input. Please try again.
-pause >nul
+echo.
+echo   [!] Invalid input. Returning to menu...
+timeout /t 2 >nul
 goto menu
 
 :sm
-echo You'll enter (Screen Mirroring Mode). Are you sure Master?:
-echo 1 for Yes
-echo 0 for No
-set /p "sm=Your choice: "
+cls
+echo.
+echo   [ SCREEN MIRRORING MODE ]
+echo.
+echo   Are you sure you want to enter this mode, Master?
+echo   [1] Yes
+echo   [0] No
+echo.
+set /p "sm=  Choice: "
 if "%sm%" == "1" goto sm1
 if "%sm%" == "0" goto menu
-cls
 if "%sm%" == "" goto sm
-echo Invalid input. Please try again.
-pause >nul
+echo.
+echo   [!] Invalid input.
+timeout /t 2 >nul
 goto sm
 
 :sm1
 cls
-C:\Users\abril\Documents\SCRCPY\scrcpy-win64-v3.3.3\scrcpy.exe -f --video-codec=h264 --max-size=1920 --video-bit-rate=19M --video-buffer=0 -M
-echo Press any key to return to menu...
-pause >nul
+if not exist "%SCRCPY_EXE%" (
+    echo [!] ERROR: scrcpy.exe not found at %SCRCPY_EXE%
+    pause
+    goto menu
+)
+"%SCRCPY_EXE%" -f --video-codec=h264 --max-size=1920 --video-bit-rate=19M --video-buffer=0 -M
+echo.
+echo   Operation finished. Returning to menu...
+timeout /t 2 >nul
 goto menu
 
 :otg
-echo You'll enter (OTG Mode). Are you sure Master?:
-echo 1 for Yes
-echo 0 for No
-set /p "otg=Your choice: "
+cls
+echo.
+echo   [ OTG MODE ]
+echo.
+echo   Are you sure you want to enter this mode, Master?
+echo   [1] Yes
+echo   [0] No
+echo.
+set /p "otg=  Choice: "
 if "%otg%" == "1" goto otg1
 if "%otg%" == "0" goto menu
-cls
 if "%otg%" == "" goto otg
-echo Invalid input. Please try again.
-pause >nul
+echo.
+echo   [!] Invalid input.
+timeout /t 2 >nul
 goto otg
 
 :otg1
 cls
-C:\Users\abril\Documents\SCRCPY\scrcpy-win64-v3.3.3\scrcpy.exe --otg
-echo Press any key to return to menu...
-pause >nul
+if not exist "%SCRCPY_EXE%" (
+    echo [!] ERROR: scrcpy.exe not found at %SCRCPY_EXE%
+    pause
+    goto menu
+)
+"%SCRCPY_EXE%" --otg
+echo.
+echo   Operation finished. Returning to menu...
+timeout /t 2 >nul
 goto menu
 
 :nw
-echo You'll enter (New Window Mode). Are you sure Master?:
-echo 1 for Yes
-echo 0 for No
-set /p "nw=Your choice: "
+cls
+echo.
+echo   [ NEW WINDOW MODE ]
+echo.
+echo   Are you sure you want to enter this mode, Master?
+echo   [1] Yes
+echo   [0] No
+echo.
+set /p "nw=  Choice: "
 if "%nw%" == "1" goto nwpick
 if "%nw%" == "0" goto menu
-cls
 if "%nw%" == "" goto nw
-echo Invalid input. Please try again.
-pause >nul
-goto nwpick
+echo.
+echo   [!] Invalid input.
+timeout /t 2 >nul
+goto nw
 
 :nwpick
 cls
-echo Here are lists of available Applications on your device:
-C:\Users\abril\Documents\SCRCPY\scrcpy-win64-v3.3.3\scrcpy.exe --list-apps
-set /p "nwpick=Paste package to open: "
-echo  Type 0 to return to menu
+if not exist "%SCRCPY_EXE%" (
+    echo [!] ERROR: scrcpy.exe not found at %SCRCPY_EXE%
+    pause
+    goto menu
+)
+echo.
+echo   Scanning available applications on your device...
+echo.
+"%SCRCPY_EXE%" --list-apps
+echo.
+echo   [0] Return to Menu
+echo.
+set /p "nwpick=  Enter package name to open: "
+if "%nwpick%" == "0" goto menu
 goto nwopen
 
 :nwopen
 cls
 if "%nwpick%" == "" (
-    echo Master, Please enter a valid package name.
-    echo Press any key to return...
-    pause >nul
+    echo.
+    echo   [!] Master, please enter a valid package name.
+    timeout /t 2 >nul
     goto nwpick
 )
-if "%nwpick%" == "0" (
-    cls
-    goto menu
-)
-C:\Users\abril\Documents\SCRCPY\scrcpy-win64-v3.3.3\scrcpy.exe -Sw -K --video-bit-rate=16M --new-display=1920x1080/210 --start-app="%nwpick%" 
-echo  Press any key to return...
-pause >nul
+"%SCRCPY_EXE%" -Sw -K --video-bit-rate=16M --new-display=1920x1080/210 --start-app="%nwpick%" 
+echo.
+echo   Operation finished. Returning to menu...
+timeout /t 2 >nul
 goto nwpick
